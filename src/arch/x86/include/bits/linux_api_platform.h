8b7e73e184b9155636529b30c76eb8ce87f174ad
Piotr Jaroszyński <p.jaroszynski@gmail.com>
2010-06-09 20:14:54 +0200

    [linux] Add the linuxlibc platform
    The linuxlibc platform is just like linux, but linked against libc.
    
    Signed-off-by: Piotr Jaroszyński <p.jaroszynski@gmail.com>

--- src/arch/x86/include/bits/linux_api_platform.h
+++ src/arch/x86/include/bits/linux_api_platform.h
@@ -1,6 +1,16 @@
 #ifndef _LINUX_API_PLATFORM_H
 #define _LINUX_API_PLATFORM_H
 
+#ifndef PLATFORM_linuxlibc
+
 extern int linux_errno;
+
+#else
+
+/* __errno_location is defined by stdlibs */
+extern int * __errno_location(void);
+#define linux_errno (*__errno_location ())
+
+#endif
 
 #endif /* _LINUX_API_PLATFORM_H */
---
+++ src/config/defaults/linuxlibc.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_DEFAULTS_LINUXLIBC_H
+#define CONFIG_DEFAULTS_LINUXLIBC_H
+
+/** @file
+ *
+ * Configuration defaults for linuxlibc
+ *
+ */
+
+/* Use linux defaults */
+#include <config/defaults/linux.h>
+
+#endif /* CONFIG_DEFAULTS_LINUXLIBC_H */
