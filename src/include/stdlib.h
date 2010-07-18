e08c3a726b12600a392d5459223a81ba89c26761
Piotr Jaroszyński <p.jaroszynski@gmail.com>
2010-07-18 17:52:43 +0200

    [misc] Add strtoull()
    Don't implement strtoul() on top of strtoull() as strtoull() is much
    bigger and only used on linux currently. Also put it in a separate
    object so it won't get linked in.
    
    Signed-off-by: Piotr Jaroszyński <p.jaroszynski@gmail.com>

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -14,6 +14,8 @@ FILE_LICENCE ( GPL2_OR_LATER );
  */
 
 extern unsigned long strtoul ( const char *p, char **endp, int base );
+extern unsigned long long strtoull ( const char *p, char **endp, int base );
+
 
 /*****************************************************************************
  *
